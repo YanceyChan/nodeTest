d8ff2389f9fea86a10eb6ae883aeb60bce975329
Yancey_Chan <chenyuanshan@outlook1120.com>
2017-06-29 18:08:53 +0800

    no message

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 'use static';
 const http = require('http');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-function start(){
+function start(route, handle){
   const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!!\n');
+    var postData = "";
+    const pathName = url.parse(req.url).pathname;
+    const query = url.parse(req.url).query;
+    console.log('Request ' + pathName + '\n' + 'Query: ' + query + '\n');
+
+    req.setEncoding("utf-8");
+    req.addListener("data", function(postDataChunk) {
+      postData += postDataChunk;
+      console.log("Received POST data chunk '"+
+      postDataChunk + "'.");
+    });
+
+    req.addListener("end", function() {
+      route(handle, pathName, res, postData);
+    });
+
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.end('Hello World!!\n');
   });
 
   server.listen(port, hostname, () => {
